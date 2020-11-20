ceb5526a4570eacf6ea474fd2dbf4bd780572b4b
Zeyltar <florian.poinsot@ynov.com>
2020-11-20 17:16:06 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StReaderWithVideo
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DateTime startTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+            St.Visibility = Visibility.Hidden;
+            Video.Source = new Uri(@"..\..\The.Mandalorian.S02E01.mkv", UriKind.Relative);
+            startTime = DateTime.Now;
+            InitialiseSubtitles(@"..\..\The.Mandalorian.S02E01.srt");
+        }
+
+        async void InitialiseSubtitles(string path)
+        {
+            Subtitle.ReadSrtFile(path);
+            await ManageSubtitle();
+        }
+
+        async Task ManageSubtitle()
+        {
+            for (int i = 0; i < Subtitle.List.Count; i++)
+            {
+                await DisplaySubtitle(i);
+                await ClearSubtitle(i);
+            }
+        }
+
+        async Task DisplaySubtitle(int index)
+        {
+            while (DateTime.Now - startTime < Subtitle.List[index].Start)
+            {
+                await Task.Delay(10);
+            }
+
+            St.FontStyle = (Subtitle.List[index].IsItalic) ? FontStyles.Italic : FontStyles.Normal;
+            
+            St.Visibility = Visibility.Visible;
+            St.Text = Subtitle.List[index].Text;
+        }
+
+        async Task ClearSubtitle(int index)
+        {
+            while (DateTime.Now - startTime < Subtitle.List[index].End)
+            {
+                await Task.Delay(10);
+            }
+            St.Visibility = Visibility.Hidden;
+            St.Text = "";
+        }
+
+    }
+}
---
+++ Subtitle.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StReaderWithVideo
+{
+    class Subtitle
+    {
+        public static List<Subtitle> List = new List<Subtitle>();
+
+        private bool isItalic_;
+        public bool IsItalic
+        {
+            get { return isItalic_;  }
+        }
+        private TimeSpan start_;
+        public TimeSpan Start
+        {
+            get { return start_; }
+        }
+
+        private TimeSpan end_;
+        public TimeSpan End
+        {
+            get { return end_; }
+        }
+
+        private string text_;
+        public string Text
+        {
+            get { return text_; }
+        }
+
+        private Subtitle(TimeSpan start, TimeSpan end, string text, bool isItalic = false)
+        {
+            start_ = start;
+            end_ = end;
+            text_ = text;
+            isItalic_ = isItalic;
+            List.Add(this);
+        }
+
+        public static void ReadSrtFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                if(Path.GetExtension(path) != ".srt")
+                    throw new Exception("error with file extension: " + Path.GetExtension(path));
+                string l = "";
+                List<string> data = new List<string>();
+                while ((l = sr.ReadLine()) != null)
+                {
+                    if (IsEmptyOrAllSpaces(l))
+                    {
+                        CreateSubtitleFromData(data);
+                        data.Clear();
+                    }
+                    else
+                        data.Add(l);
+                }
+                
+            }
+        }
+
+        private static bool IsEmptyOrAllSpaces(string str)
+        {
+            return null != str && str.All(c => c.Equals(' '));
+        }
+
+        private static void CreateSubtitleFromData(List<string> data)
+        {
+            TimeSpan start = new TimeSpan(0, 0, 0);
+            TimeSpan end = new TimeSpan(0, 0, 0);
+            string text = "";
+            int index;
+
+            if (!int.TryParse(data[0], out index))
+                throw new Exception("error on subtitle: " + data[0] + " is not an integer");
+
+            if (data[1].Contains("-->"))
+            {
+                string[] sptS = data[1].Split(new string[] { " --> " }, StringSplitOptions.None);
+                if (TimeSpan.TryParse(sptS[0], out TimeSpan result1) && TimeSpan.TryParse(sptS[1], out TimeSpan result2))
+                {
+                    start = result1;
+                    end = result2;
+                }
+                else
+                    throw new Exception("error on subtitle timing at " + index + " : " + data[1]);
+            }
+
+            for (int i = 2; i < data.Count; i++)
+            {
+                if (text.Length > 0)
+                    text += '\n';
+                text += data[i];
+            }
+
+            bool italic;
+            if (italic = IsItalicText(text))
+                text = text.Replace("<i>", "").Replace("</i>", "");
+
+            new Subtitle(start, end, text, italic);
+        }
+
+        private static bool IsItalicText(string text)
+        {
+            return text.Contains("<i>");
+
+        }
+    }
+}
